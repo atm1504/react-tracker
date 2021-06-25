8f0da804f64d5042c724e8f02c1a6bd00ccd402e
atm1504 <atm1504.in@gmail.com>
2021-06-25 17:27:27 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react";
+import AddTask from "./components/AddTask";
+
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -22,6 +25,13 @@ function App() {
       reminder: true,
     },
   ]);
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+    console.log(tasks);
+  };
   const deleteTask = (id) => {
     console.log("Deleted ", id);
     setTasks(tasks.filter((task) => task.id !== id));
@@ -39,6 +49,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
