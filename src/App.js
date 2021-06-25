42a251b8ea29d19f0386d700bb690a41d1fd7247
Amartya Mondal <atm1504.in@gmail.com>
2021-06-25 15:22:42 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import Header from "./components/Header";
-
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Food shopping",
+      day: "Feb 5th at 2:30Pm",
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: "Ecom shopping",
+      day: "Feb 6th at 2:30Pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Dig shopping",
+      day: "Feb 7th at 2:30Pm",
+      reminder: true,
+    },
+  ]);
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Task from "./Task";
+// const tasks = [
+//   {
+//     id: 1,
+//     text: "Food shopping",
+//     day: "Feb 5th at 2:30Pm",
+//     reminder: false,
+//   },
+//   {
+//     id: 2,
+//     text: "Ecom shopping",
+//     day: "Feb 6th at 2:30Pm",
+//     reminder: true,
+//   },
+//   {
+//     id: 3,
+//     text: "Dig shopping",
+//     day: "Feb 7th at 2:30Pm",
+//     reminder: true,
+//   },
+// ];
+
+const Tasks = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task task={task} key={task.id} />
+      ))}
+    </>
+  );
+};
+
+export default Tasks;
